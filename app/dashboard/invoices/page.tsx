a7d547eb55ed95e3f3167117060e7fa85c0f3946
itstahirrafique <hello.tahirrafique@gmail.com>
2025-04-16 12:29:02 +0500

    initalzied a new repo

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <Link href="/">Go HOme</Link>
+      <p>Invoices Page</p>
+    </div>
+  );
+}
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,4 +1,5 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+
 import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
@@ -18,12 +19,14 @@ export default function InvoiceStatus({ status }: { status: string }) {
           <ClockIcon className="ml-1 w-4 text-gray-500" />
         </>
       ) : null}
+
       {status === 'paid' ? (
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
+      
     </span>
   );
 }
